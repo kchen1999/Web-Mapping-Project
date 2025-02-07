d37614b57236cddf92c9ced2153be9ca4dc7c408
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2025-02-07 21:52:03 +1100

    fixed mergesort bug

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -70,6 +70,9 @@ public class MergeSort {
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
+        if (items.isEmpty()) {
+            return items;
+        }
         Queue<Queue<Item>> queue = makeSingleItemQueues(items);
         while (queue.size() != 1) {
             Queue<Item> q1 = queue.dequeue();
